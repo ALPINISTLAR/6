e6832c09c8e0fe90a4e2e7b48d7ebcc072e2340c
ALPINISTLAR <niveam424@gmail.com>
2024-05-24 07:21:16 +0200

    change selectedBatchIndex opacity

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -21,56 +21,60 @@ ChartJS.register(
   Title,
   Tooltip,
   Legend
-);
-
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      display: false,
-    },
-  },
-};
-
-export function Chart() {
-  const [selectedBatchIndex, setSelectedBatchIndex] = useState(0);
-
-  const handleBatchClick = useCallback(index => {
-    setSelectedBatchIndex(index);
-  }, []);
-
-  const batch = jsonData.batchList[selectedBatchIndex];
-
-  const labels = [];
-  const rates = [];
-
-  batch.rates.forEach((rate, index) => {
-    labels.push(new Date(batch.startTime + index * batch.interval));
-    rates.push(rate);
-  });
-
-  const dataset = {
-    label: `Batch ${selectedBatchIndex + 1}`,
-    data: rates,
-    borderColor: 'rgb(0, 113, 235)',
-    backgroundColor: 'rgb(0, 113, 235)',
-  };
-
-  const data = {
-    labels,
-    datasets: [dataset],
-  };
-
-  return (
-    <div>
-      <div className='header'>
-        {jsonData.batchList.map((batch, index) => (
-          <button key={index} onClick={() => handleBatchClick(index)}>
-            {batch.name}
-          </button>
-        ))}
-      </div>
-      <Line options={options} data={data} />
-    </div>
   );
-}
+
+  export const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+
+  export function Chart() {
+    const [selectedBatchIndex, setSelectedBatchIndex] = useState(0);
+
+    const handleBatchClick = useCallback(index => {
+      setSelectedBatchIndex(index);
+    }, []);
+
+    const batch = jsonData.batchList[selectedBatchIndex];
+
+    const labels = [];
+    const rates = [];
+
+    batch.rates.forEach((rate, index) => {
+      labels.push(new Date(batch.startTime + index * batch.interval));
+      rates.push(rate);
+    });
+
+    const dataset = {
+      label: `Batch ${selectedBatchIndex + 1}`,
+      data: rates,
+      borderColor: 'rgb(0, 113, 235)',
+      backgroundColor: 'rgb(0, 113, 235)',
+    };
+
+    const data = {
+      labels,
+      datasets: [dataset],
+    };
+
+    return (
+      <div>
+      <div className='header'>
+      {jsonData.batchList.map((batch, index) => (
+        <button
+        key={index}
+        onClick={() => handleBatchClick(index)}
+        style={{ opacity: selectedBatchIndex === index ? 0.6 : 1 }}
+        >
+        {batch.name}
+        </button>
+        ))}
+        </div>
+        <Line options={options} data={data} />
+        </div>
+        );
+      }
